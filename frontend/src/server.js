0ea719c6bdedbed12bab5948e791504a93a2eb9f
Ayobami <tnenas07@gmail.com>
2023-07-07 19:24:24 +0100

    backend url added

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,6 @@
 // export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
-export const server = 'http://localhost:8000/api/v2';
+// export const server = 'http://localhost:8000/api/v2';
+export const server = 'https://e-shop1.vercel.app/api/v2';
 
 // export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
 export const backend_url = 'http://localhost:8000/';
