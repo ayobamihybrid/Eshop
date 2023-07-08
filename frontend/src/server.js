e3e87712c05358279741a1c22424c218d03b0e19
Ayobami <tnenas07@gmail.com>
2023-07-08 13:17:08 +0100

    updating server url in the frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,7 @@
 // export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
 // export const server = 'http://localhost:8000/api/v2';
-// export const server = 'https://e-shop1.vercel.app/api/v2';
-export const server = 'https://ecommerceshop-eyag.onrender.com/api/v2';
+export const server = 'https://eshop-7fwi.onrender.com/api/v2';
 
 // export const backend_url = 'http://localhost:8000/';
-export const backend_url = 'https://ecommerceshop-eyag.onrender.com/';
+// export const backend_url = 'https://eshop-7fwi.onrender.com';
+export const backend_url = 'https://eshop-7fwi.onrender.com';
